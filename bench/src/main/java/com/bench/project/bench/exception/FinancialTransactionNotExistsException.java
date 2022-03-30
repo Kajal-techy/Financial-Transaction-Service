a429fe5968f1716fee457155a99806291ca38034
Kajal-techy <59594311+Kajal-techy@users.noreply.github.com>
2022-03-30 11:35:13 -0400

    Delete bench directory

--- bench/src/main/java/com/bench/project/bench/exception/FinancialTransactionNotExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.bench.project.bench.exception;
-
-public class FinancialTransactionNotExistsException extends Exception {
-
-    public FinancialTransactionNotExistsException() {
-        super("Financial Transaction Does not exists");
-    }
-}
